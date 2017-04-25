6e583492c9b3a381d1ea7eaad29b7d1a5ce3b489
HackerWilson <wenbinwb1@foxmail.com>
2017-04-25 22:51:42 +0800

    评价数及功能正常显示

--- lectures/views.py
+++ lectures/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.db.models import Avg
 from django.shortcuts import render
 
 from .models import Category, Lecture
+from feedbacks.models import Comment
 
 
 def list_categories(request):
@@ -13,7 +15,7 @@ def list_categories(request):
 
 
 def list_lectures(request, category_id):
-    lecture_list = Lecture.objects.filter(category__order=category_id, meeting__is_offline=False, is_finished=False)\
+    lecture_list = Lecture.objects.filter(category__order=category_id, meeting__is_offline=False)\
                        .order_by('-time')\
                        .values('id', 'topic', 'time', 'meeting__number', 'speaker__name', 'speaker__intro', 'speaker__picture')
     context = {'lecture_list': lecture_list}
@@ -23,5 +25,12 @@ def list_lectures(request, category_id):
 def lecture_detail(request, lecture_id):
     user = request.user
     lecture = Lecture.objects.get(id=lecture_id)
-    context = {'user': user, 'lecture': lecture}
+
+    comment_qs = Comment.objects.filter(lecture__id=lecture_id)
+    comment_num = comment_qs.count()
+    comment_agg =  comment_qs.aggregate(comment_avg_score=Avg('score'))
+
+    context = {'user': user, 'lecture': lecture,
+               'comment_num': comment_num,
+               'comment_avg_score': comment_agg['comment_avg_score']}
     return render(request, 'lectures/lecture.html', context)
